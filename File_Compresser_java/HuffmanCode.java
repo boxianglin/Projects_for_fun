2d55f766ea76b248b97852c6d3cfc5013077568a
Boxiang Lin <documentinglin@gmail.com>
2021-05-19 17:21:22 -0700

    encode

--- File_Compresser_java/HuffmanCode.java
+++ File_Compresser_java/HuffmanCode.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -9,12 +10,48 @@ public class HuffmanCode {
 	public static void main(String[] args) {
 		String content = "i like like like java do you like a java";
 		byte [] contentBytes = content.getBytes();
-		System.out.print(contentBytes.length);
-
+		System.out.println(contentBytes.length);
+		
+ 
+		
+		byte[] compressed = huffmanZip(contentBytes);
+		System.out.println(Arrays.toString(compressed) + "size: " + compressed.length);
+		
+		
+		 
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param bytes [the original bytes array]
+	 * @return compressed bytes array (by huffmancode)
+	 */
+	
+	private static byte[] huffmanZip(byte[] bytes) {
+		
+		//convert to a list of nodes
+		List <Node> nodes = getNodes(bytes);
+		
+		//using list of nodes to generate the huffmanTree structure
+		Node huffmanTreeRoot = createHumanTree(nodes);
+		huffmanTreeRoot.preOrder();
+	 
+		
+		//using huffmanTree to generate the huffmanCode (stored in a map)
+		Map<Byte, String> huffmanCodes = getCodes(huffmanTreeRoot);
+		System.out.println(huffmanCodes);
+		
+		//store the huffmancode to the new byte array
+		byte[] huffmanCodeBytes = zip(bytes, huffmanCodes);
+		
+		return huffmanCodeBytes;
 	}
 	
 	
 	
+ 
 	
 	/**
 	 * 
@@ -72,6 +109,92 @@ public class HuffmanCode {
 		return nodes.get(0);
 	}
 	
+	
+	
+	
+	/**
+	 * 
+	 * @param bytes  //original byte codes
+	 * @param huffmanCodes   //the huffmancodes
+	 * @return huffmancodeByte = [index] = 8bits (in 2's binary) ¡¾inverse bits and plus 1¡¿
+	 */
+	private static byte[] zip (byte[] bytes, Map<Byte, String> huffmanCodes) {
+		
+		//make the original bytes [] to string huffmancodes
+		StringBuilder stringBuilder = new StringBuilder();
+		for(byte b : bytes) {
+			stringBuilder.append(huffmanCodes.get(b));
+		}
+		
+		int len;
+		if(stringBuilder.length() % 8 == 0) {
+			len = stringBuilder.length()/8;
+		}else {
+			len = stringBuilder.length()/8 + 1;
+		}
+		
+		byte[] huffmancodeByte = new byte[len];
+		
+		int index = 0;
+		for(int i = 0; i<stringBuilder.length(); i=i+8) {
+			
+			String  strByte;
+			
+			if(i+8 > stringBuilder.length()) {
+				strByte = stringBuilder.substring(i);
+			}else {
+				strByte = stringBuilder.substring(i,i+8);
+			}
+			huffmancodeByte[index] = (byte)Integer.parseInt(strByte,2);
+			index++;	
+		}
+		return huffmancodeByte;
+		
+	}
+	
+	
+	/*
+	 * MAP to store the huffman code tablle
+	 */
+	static Map<Byte, String> huffmanCodes = new HashMap<>();
+	
+	/*
+	 * Use stringbuilder collect the path(code) from huffmanTree
+	 */
+	
+	static StringBuilder stringBuilder = new StringBuilder();
+	
+	
+	private static Map<Byte, String> getCodes(Node root){
+		if(root == null) {
+			return null;
+		}
+		getCodes(root, "", stringBuilder); 
+		return huffmanCodes;
+	}
+	
+	/**
+	 * Function: store the huffmancode by the path from node root to leaf to the map
+	 *           left = 0, right = 1;
+	 * @param node 
+	 * @param code 
+	 * @param stringBuilder
+	 */
+	private static void getCodes(Node node, String code, StringBuilder stringBuilder) {
+		StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+		//code+strBuilde2 
+		stringBuilder2.append(code);
+		if(node != null) {
+			//non-leaf node --- code recorded
+			if(node.data == null) {
+				getCodes(node.left, "0", stringBuilder2);
+				getCodes(node.right, "1", stringBuilder2);
+			}else {
+				//leaf node, put to the huffmancode map
+				huffmanCodes.put(node.data, stringBuilder2.toString());
+			}
+		}
+	}
 	
 	
 	
